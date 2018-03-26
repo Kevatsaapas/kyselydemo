a6db017ddf8c3bfb8fc5aa92e542dfe9d008c4a7
Mikko Maja <mikmaj88@gmail.com>
2018-03-26 14:01:47 +0300

    Initial commit

---
+++ src/main/java/com/kevatsaapas/kyselydemo/KyselydemoApplication.java
@@ -0,0 +1,12 @@
+package com.kevatsaapas.kyselydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KyselydemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KyselydemoApplication.class, args);
+	}
+}
